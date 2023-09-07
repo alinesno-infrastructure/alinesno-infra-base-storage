2382d184f7ea25070a4ea4f499c770f6ba0b281a
switchII <luoandon@gmail.com>
2023-09-08 01:39:18 +0800

    feat:集成发布

--- alinesno-infra-base-storage-boot/src/main/java/com/alinesno/infra/base/storage/BaseStorageApplication.java
+++ alinesno-infra-base-storage-boot/src/main/java/com/alinesno/infra/base/storage/BaseStorageApplication.java
@@ -1,6 +1,7 @@
 package com.alinesno.infra.base.storage;
 
 import cn.xuyanwu.spring.file.storage.spring.EnableFileStorage;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
@@ -11,9 +12,10 @@ import org.springframework.context.annotation.EnableAspectJAutoProxy;
  * @author LuoAnDong
  * @since 2023年8月3日 上午6:23:43
  */
+@MapperScan("com.alinesno.infra.base.storage.mapper")
 @EnableAspectJAutoProxy
 @EnableFileStorage
-@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+@SpringBootApplication
 public class BaseStorageApplication {
 
 	public static void main(String[] args) {
