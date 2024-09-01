29a34c58f331eb4262538d23de2e3148ca2a4439
赵阳(Yang) <yangAip@gmail.com>
2024-09-01 18:05:00 -0400

    修复FileType.java文件中Office文件类型数组拼接错误，并更新为正确的Office文件类型列表。

--- alinesno-infra-base-storage-fileshow/src/main/java/com/alinesno/infra/base/storage/core/model/FileType.java
+++ alinesno-infra-base-storage-fileshow/src/main/java/com/alinesno/infra/base/storage/core/model/FileType.java
@@ -35,7 +35,7 @@ public enum FileType {
     DCM("dcmFilePreviewImpl"),
     DRAWIO("drawioFilePreviewImpl");
 
-    private static final String[] OFFICE_TYPES = {"docx", "wps", "doc", "docm", "xls", "xlsx", "csv" ,"xlsm", "ppt", "pptx", "vsd", "rtf", "odt", "wmf", "emf", "dps", "et", "ods", "ots", "tsv", "odp", "otp", "sxi", "ott", "vsdx", "fodt", "fods", "xltx","tga","psd","dotm","ett","xlt","xltm","wpt","dot","xlam","dotx","xla","pages", "eps"};
+    private static final String[] OFFICE_TYPES = {"docx", "wps", "doc", "docm", "xls", "xlsx", "csv" ,"xlsm", "ppt", "pptx", "vsd", "rtf", "odt", "wmf", "emf", "dps", "et", "ods", "ots", "tsv", "odp", "otp", "sxi", "ott", "vsdx", "fodt", "fods", "xltx","tga","psd","dotm","ett","xlt","xltm","wpt","dot","xlam","dotx","xla", "pages", "eps"};
     private static final String[] PICTURE_TYPES = {"jpg", "jpeg", "png", "gif", "bmp", "ico", "jfif", "webp"};
     private static final String[] ARCHIVE_TYPES = {"rar", "zip", "jar", "7-zip", "tar", "gzip", "7z"};
     private static final String[] ONLINE3D_TYPES = {"obj", "3ds", "stl", "ply", "off", "3dm", "fbx", "dae", "wrl", "3mf", "ifc","glb","o3dv","gltf","stp","bim","fcstd","step","iges","brep"};
