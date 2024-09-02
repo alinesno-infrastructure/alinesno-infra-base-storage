df60b3593af1b216b1fdf9e0a0fcad2f3e0f6f2e
王磊(Levi) <leviAip@gmail.com>
2024-09-02 09:35:00 -0400

    新增FileRecordMapper接口。

---
+++ alinesno-infra-base-storage-domain/src/main/java/com/alinesno/infra/base/storage/mapper/FileRecordMapper.java
@@ -0,0 +1,8 @@
+package com.alinesno.infra.base.storage.mapper;
+
+import com.alinesno.infra.base.storage.entity.FileRecordEntity;
+import com.alinesno.infra.common.facade.mapper.repository.IBaseMapper;
+
+public interface FileRecordMapper extends IBaseMapper<FileRecordEntity> {
+
+}
