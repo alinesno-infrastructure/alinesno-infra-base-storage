120deecd4705431cfd0d526b200061e1c222e35f
王磊(Levi) <leviAip@gmail.com>
2024-09-02 02:24:00 -0400

    fix:优化StorageProjectServiceImpl，添加默认应用初始化及文档类型更新逻辑。

--- alinesno-infra-base-storage-domain/src/main/java/com/alinesno/infra/base/storage/service/impl/StorageProjectServiceImpl.java
+++ alinesno-infra-base-storage-domain/src/main/java/com/alinesno/infra/base/storage/service/impl/StorageProjectServiceImpl.java
@@ -1,21 +1,31 @@
 package com.alinesno.infra.base.storage.service.impl;
 
+import cn.hutool.json.JSONUtil;
+import com.alinesno.infra.base.storage.api.UpdateDocumentTypeDto;
+import com.alinesno.infra.base.storage.entity.DocumentTypeEntity;
 import com.alinesno.infra.base.storage.entity.StorageProjectEntity;
 import com.alinesno.infra.base.storage.enums.DocumentTypeEnum;
 import com.alinesno.infra.base.storage.mapper.StorageProjectMapper;
+import com.alinesno.infra.base.storage.service.IDocumentTypeService;
 import com.alinesno.infra.base.storage.service.IStorageProjectService;
 import com.alinesno.infra.common.core.service.impl.IBaseServiceImpl;
 import lombok.extern.slf4j.Slf4j;
+import net.minidev.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.sqids.Sqids;
 
 import java.util.Arrays;
+import java.util.List;
 
 @Slf4j
 @Service
 public class StorageProjectServiceImpl extends IBaseServiceImpl<StorageProjectEntity, StorageProjectMapper> implements IStorageProjectService {
 
     private static final String DEFAULT_PROJECT_FIELD = "default" ;
+
+    @Autowired
+    private IDocumentTypeService documentTypeService ;
 
     @Override
     public void initDefaultApp(long userId) {
@@ -30,8 +40,24 @@ public class StorageProjectServiceImpl extends IBaseServiceImpl<StorageProjectEn
         project.setProjectName("默认存储应用");
         project.setProjectDesc("包含所有的上传存储权限，用于开发和验证场景");
         project.setProjectCode(code);
-        project.setDocumentType(DocumentTypeEnum.getAllNameStr());
+
+        List<DocumentTypeEntity> documentTypes = documentTypeService.initDocumentType(userId);
+        List<String> ids = documentTypes.stream().map(DocumentTypeEntity::getId).toList()
+                .stream().map(Object::toString).toList(); // fix:处理前端转换精度的问题
+
+        project.setDocumentType(JSONUtil.toJsonStr(ids)) ;
 
         save(project) ;
+    }
+
+    @Override
+    public int updateDocumentType(UpdateDocumentTypeDto dto) {
+        StorageProjectEntity project = getById(Long.parseLong(dto.getProjectId()));
+        if (project != null) {
+            project.setDocumentType(JSONUtil.toJsonStr(dto.getDocumentIds()));
+            updateById(project);
+            return 1;
+        }
+        return 0;
     }
 }
