667981676c7ee09ffdc92030f6760f4adc046fb6
王磊(Levi) <leviAip@gmail.com>
2024-09-02 10:27:00 -0400

    新增BackupRecordMapper接口文件。

---
+++ alinesno-infra-base-storage-domain/src/main/java/com/alinesno/infra/base/storage/mapper/BackupRecordMapper.java
@@ -0,0 +1,7 @@
+package com.alinesno.infra.base.storage.mapper;
+
+import com.alinesno.infra.base.storage.entity.BackupRecordEntity;
+import com.alinesno.infra.common.facade.mapper.repository.IBaseMapper;
+
+public interface BackupRecordMapper extends IBaseMapper<BackupRecordEntity> {
+}
