29bef0d3f995cd69b2a63df3ad72776409cfaa26
赵阳(Yang) <yangAip@gmail.com>
2024-09-02 04:57:00 -0400

    修复了SimpleEncodingDetects类中的资源读取异常问题。

--- alinesno-infra-base-storage-fileshow/src/main/java/com/alinesno/infra/base/storage/core/utils/SimpleEncodingDetects.java
+++ alinesno-infra-base-storage-fileshow/src/main/java/com/alinesno/infra/base/storage/core/utils/SimpleEncodingDetects.java
@@ -1,11 +1,6 @@
 package com.alinesno.infra.base.storage.core.utils;
 
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.URL;
 
 /**
